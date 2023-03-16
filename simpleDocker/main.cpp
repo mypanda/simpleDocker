02f550428d21fc1513d30b9e3479afc3a02dc3b7
mypanda <951788234@qq.com>
2023-03-16 15:43:49 +0800

    Update main.cpp

--- simpleDocker/main.cpp
+++ simpleDocker/main.cpp
@@ -12,9 +12,9 @@ int main(int argc, char** argv) {
     docker::container_config config;
     config.host_name = "kaixindeken";
     config.root_dir  = "./kaixindeken";
-    config.ip        = "192.168.0.100";
+    config.ip        = "172.16.0.100";
     config.bridge_name = "docker0";
-    config.bridge_ip   = "192.168.0.1";
+    config.bridge_ip   = "172.16.0.1";
     docker::container container(config);
     container.start();
     std::cout << "stop container..." << std::endl;
